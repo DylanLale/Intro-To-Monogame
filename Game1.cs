c8952694d15774dcb3e92baa28e60982505b66a8
DylanLale <disneygamer706@gmail.com>
2022-11-15 09:32:10 -0500

    DONE

--- Game1.cs
+++ Game1.cs
@@ -14,6 +14,8 @@ namespace Intro_To_Monogame
         Texture2D HulkTexture;
         Texture2D SpiderManTexture;
         Texture2D AirportTexture;
+        Texture2D ScarletWitchTexture;
+        Texture2D BlackPantherTexture;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -36,6 +38,8 @@ namespace Intro_To_Monogame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            BlackPantherTexture = Content.Load<Texture2D>("Black Panther");
+            ScarletWitchTexture = Content.Load<Texture2D>("Scarlet Witch");
             CapAmericaTexture = Content.Load<Texture2D>("Captain America");
             IronManTexture = Content.Load<Texture2D>("Iron Man");
             HulkTexture = Content.Load<Texture2D>("Hulk");
@@ -61,11 +65,13 @@ namespace Intro_To_Monogame
             _spriteBatch.Begin();
 
             _spriteBatch.Draw(AirportTexture, new Vector2(0, 0), Color.White);
-            _spriteBatch.Draw(IronManTexture,new Vector2(550, 50),Color.White);
-            _spriteBatch.Draw(CapAmericaTexture, new Vector2(100, 330), Color.White);
+            _spriteBatch.Draw(SpiderManTexture, new Vector2(525, 200), Color.White);
+            _spriteBatch.Draw(BlackPantherTexture, new Vector2(550, 320), Color.White);
+            _spriteBatch.Draw(IronManTexture,new Vector2(650, 50),Color.White);
+            _spriteBatch.Draw(CapAmericaTexture, new Vector2(25, 330), Color.White);
             _spriteBatch.Draw(HulkTexture, new Vector2(200, 50), Color.White);
-            _spriteBatch.Draw(SpiderManTexture, new Vector2(550, 200), Color.White);
-            
+            _spriteBatch.Draw(ScarletWitchTexture, new Vector2(175, 300), Color.White);
+
 
             _spriteBatch.End();
             base.Draw(gameTime);
